510db91870ceb041441d11a49358b4320acc37a6
JulijaKauh <122730979+JulijaKauh@users.noreply.github.com>
2023-02-11 16:40:06 +0100

    Update random_book.py

--- random_book.py
+++ random_book.py
@@ -5,8 +5,7 @@ books = [["mystery", "The Paris Apartment"],
          ["suspense", "The Huntress"],
          ["crime", "A Good Girl's Guide to Murder"]]
 
-print("What genre do  you want  to read?")
-genre = input()
+genre = input("What genre do  you want  to read? ")
 for i in books:
     if i[0] == genre:
         print('Genre: ' + genre + '.' + ' Book: ' + i[1] + '.')

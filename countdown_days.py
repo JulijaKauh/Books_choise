04ea6493bba9648df146261f191a2f8cd39dd819
JulijaKauh <122730979+JulijaKauh@users.noreply.github.com>
2023-02-12 14:26:10 +0100

    countdown_days

---
+++ countdown_days.py
@@ -0,0 +1,16 @@
+import datetime
+print("When does your project start: ")
+year1 = int(input("Year: "))
+month1 = int(input("Month: "))
+day1 = int(input("Day: "))
+
+print("When does your project end: ")
+year2 = int(input("Year: "))
+month2 = int(input("Month: "))
+day2 = int(input("Day: "))
+
+
+start_project = datetime.date(year1, month1, day1)
+end_project = datetime.date(year2, month2, day2)
+count_of_days = start_project - end_project
+print("Start: {}. Ends {}. It takes {} day/s.".format(start_project, end_project, abs(count_of_days.days)))

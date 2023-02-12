46d57e524c0e5883e48028c6f8bc6de3381e9cfa
JulijaKauh <122730979+JulijaKauh@users.noreply.github.com>
2023-02-12 14:31:50 +0100

    amount of days between two dates

--- countdown_days.py
+++ countdown_days.py
@@ -1,15 +1,15 @@
 import datetime
+#Project starts:
 print("When does your project start: ")
 year1 = int(input("Year: "))
 month1 = int(input("Month: "))
 day1 = int(input("Day: "))
-
+#Project ends:
 print("When does your project end: ")
 year2 = int(input("Year: "))
 month2 = int(input("Month: "))
 day2 = int(input("Day: "))
-
-
+#amount of days between them
 start_project = datetime.date(year1, month1, day1)
 end_project = datetime.date(year2, month2, day2)
 count_of_days = start_project - end_project
